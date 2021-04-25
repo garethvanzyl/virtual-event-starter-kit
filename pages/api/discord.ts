ef757464fd27463bd90b4499ab708406f47725a9
System Administrator <gareth@foxstreetcomms.co.za>
2021-04-25 20:43:49 +0200

    added new bot token

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -22,7 +22,7 @@ interface ReactionSelector {
 }
 
 // After creating a bot, add the token as an environment var
-const { DISCORD_BOT_TOKEN } = `Nzk0NDk2MDMzMzIwOTkyNzY5.X-7qFw.yy8MemlpZQbw9TEVn0yW-lg8Hjc`;
+const { DISCORD_BOT_TOKEN } = `Nzk0NDk2MDMzMzIwOTkyNzY5.X-7qFw.nxRO2b1lqxvk_scCQFRYWwdmkqg`;
 
 // Number of seconds to cache the API response for
 const EXPIRES_SECONDS = 60;
